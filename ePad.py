adda2de0db2dd7da9457c5b94ec21712b1a5b70d
Jeff Hoogland <jeffhoogland@linux.com>
2014-03-22 17:28:38 -0500

    Open and new file features now working

--- ePad.py
+++ ePad.py
@@ -29,6 +29,11 @@ from efl.elementary.fileselector import Fileselector, \
     ELM_FILESELECTOR_SORT_BY_TYPE_ASC, ELM_FILESELECTOR_SORT_BY_TYPE_DESC, \
     ELM_FILESELECTOR_SORT_BY_SIZE_ASC, ELM_FILESELECTOR_SORT_BY_SIZE_DESC, \
     ELM_FILESELECTOR_SORT_BY_MODIFIED_ASC, ELM_FILESELECTOR_SORT_BY_MODIFIED_DESC
+from efl.elementary.transit import Transit, ELM_TRANSIT_EFFECT_WIPE_TYPE_HIDE, \
+    ELM_TRANSIT_EFFECT_WIPE_DIR_RIGHT, ELM_TRANSIT_EFFECT_FLIP_AXIS_X, \
+    ELM_TRANSIT_EFFECT_FLIP_AXIS_Y, ELM_TRANSIT_TWEEN_MODE_ACCELERATE, \
+    ELM_TRANSIT_TWEEN_MODE_DECELERATE, TransitCustomEffect
+
 
 EXPAND_BOTH = EVAS_HINT_EXPAND, EVAS_HINT_EXPAND
 EXPAND_HORIZ = EVAS_HINT_EXPAND, 0.0
@@ -38,7 +43,7 @@ ALIGN_CENTER = 0.5, 0.5
 
 class Interface(object):
     def __init__( self ):
-        self.mainWindow = StandardWindow("ePad", "ePad - Untitled", autodel=True, size=(600, 400))
+        self.mainWindow = StandardWindow("ePad", "Untitled - ePad", autodel=True, size=(600, 400))
         self.mainWindow.callback_delete_request_add(self.closeChecks)
 
         self.mainBox = Box(self.mainWindow, size_hint_weight=EXPAND_BOTH, size_hint_align=FILL_BOTH)
@@ -53,6 +58,7 @@ class Interface(object):
         self.mainTb.item_append("dialog-information", "About", self.aboutPress)
 
         self.mainEn = Entry(self.mainWindow, size_hint_weight=EXPAND_BOTH, size_hint_align=FILL_BOTH)
+        self.mainEn.callback_changed_user_add(self.textEdited)
         self.mainEn.scrollable_set(True) # creates scrollbars rather than enlarge window
         self.mainEn.line_wrap_set(False) # does not allow line wrap (can be changed by user)
         self.mainEn.autosave_set(False) # set to false to reduce disk I/O
@@ -80,7 +86,10 @@ class Interface(object):
         self.mainWindow.resize_object_add(self.flip)
         self.flip.show()
 
+        self.isSaved = False
+
     def newPress( self, obj, it ):
+        self.newFile()
         it.selected_set(False)
 
     def openPress( self, obj, it ):
@@ -91,7 +100,7 @@ class Interface(object):
         if self.mainEn.file_get()[0] == None:
             self.saveAs()
         else:
-            self.save()
+            self.saveFile()
         it.selected_set(False)
 
     def saveAsPress( self, obj, it ):
@@ -101,8 +110,17 @@ class Interface(object):
     def optionsPress( self, obj, it ):
         it.selected_set(False)
 
+    def textEdited( self, obj ):
+        ourFile = self.mainEn.file_get()[0]
+        if ourFile:
+            self.mainWindow.title_set("*%s - ePad"%ourFile)
+        else:
+            self.mainWindow.title_set("*Untitlted - ePad")
+        self.isSaved = False
+
     def fileSelected( self, fs, file_selected ):
         self.flip.go(ELM_FLIP_ROTATE_XZ_CENTER_AXIS)
+        print file_selected
         IsSave = fs.is_save_get()
         if IsSave:
             open(file_selected,'w').close() # creates new file
@@ -110,11 +128,11 @@ class Interface(object):
             self.mainEn.file_set(file_selected, ELM_TEXT_FORMAT_PLAIN_UTF8)
             self.mainEn.entry_set(tmp_text)
             self.mainEn.file_save()
-            self.mainWindow.title_set("Etext - "+file_selected)
+            self.mainWindow.title_set("%s - ePad" % file_selected)
+            self.isSaved = True
         else:
             self.mainEn.file_set(file_selected, ELM_TEXT_FORMAT_PLAIN_UTF8)
-            self.mainWindow.title_set("ePad - %s" % file_selected)
-        print file_selected
+            self.mainWindow.title_set("%s - ePad" % file_selected)
 
     def aboutPress( self, obj, it ):
         #About popup
@@ -130,6 +148,20 @@ class Interface(object):
     def aboutClose( self, bt ):
         self.popupAbout.delete()
 
+    def newFile( self ):
+        trans = Transit()
+        trans.object_add(self.mainEn)
+        trans.auto_reverse = True
+
+        trans.effect_wipe_add(
+            ELM_TRANSIT_EFFECT_WIPE_TYPE_HIDE,
+            ELM_TRANSIT_EFFECT_WIPE_DIR_RIGHT)
+
+        trans.duration = 0.5
+        trans.go()
+
+        self.mainEn.entry_set("")
+
     def openFile( self ):
         self.fileSelector.is_save_set(False)
         self.flip.go(ELM_FLIP_ROTATE_YZ_CENTER_AXIS)
@@ -137,6 +169,11 @@ class Interface(object):
     def saveAs( self ):
         self.fileSelector.is_save_set(True)
         self.flip.go(ELM_FLIP_ROTATE_XZ_CENTER_AXIS)
+
+    def saveFile( self ):
+        self.mainEn.file_save()
+        self.mainWindow.title_set("%s - ePad"%self.mainEn.file_get()[0])
+        self.isSaved = True
 
     def closeChecks( self, obj ):
         elementary.exit()
